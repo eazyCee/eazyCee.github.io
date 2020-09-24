17408e736879415b17843c32120eef3f7edbfca9
CeeTee <clifftangel@gmail.com>
2020-09-24 19:31:38 +0700

    tet

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,28 @@
-window.addEventListener('scroll', () => {
-	const scrolled = window.scrollY;
-
-	console.log(scrolled);
-
+$(document).ready(function (){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 100){
+            $("nav").removeClass("navTransparent");
+            $("nav").addClass("navColor");   
+        }
+        else{
+            $("nav").removeClass("navColor");
+            $("nav").addClass("navTransparent");
+        }
+    })
+     $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 100){
+            $("highest").removeClass("imgTransparent");
+        }
+        else{
+            $("nav").addClass("imgTransparent");
+        }
+    })
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 });
