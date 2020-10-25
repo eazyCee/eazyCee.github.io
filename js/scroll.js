da168d0c65cc1523351bb73171aa1d2b44b704cd
CeeTee <clifftangel@gmail.com>
2020-10-25 22:08:32 +0700

    revamp

--- js/scroll.js
+++ js/scroll.js
@@ -34,3 +34,36 @@ $(document).ready(function (){
         }, 500);
     });
 });
+
+$(document).ready(function(){
+    $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        focusOnSelect: true,
+        slidesToScroll: 3,
+        responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+});
